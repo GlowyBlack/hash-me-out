e8f76cd38d122b72dbb18e060be0ab55bba3e280
GlowyBlack <a.oludare1234@gmail.com>
2025-10-27 13:09:54 -0700

    More work on Review Features and Some test stuff

--- backend/app/services/review_service.py
+++ backend/app/services/review_service.py
@@ -1,13 +1,14 @@
 
 from pathlib import Path
+from app.models.review import Review
 from app.utils.data_manager import CSVRepository
+from app.schemas.review import ReviewCreate, ReviewRead, ReviewUpdate
 
-DATA_PATH = Path(__file__).resolve().parents[1] / "data" / "Reviews.csv"
 
 class ReviewService:
     def __init__(self):
         self.repo = CSVRepository()
-        self.path = DATA_PATH
+        self.path = Path(__file__).resolve().parents[1] / "data" / "Reviews.csv"
         self.fields = ["ReviewID", "UserID", "ISBN", "Comment", "Time"]
         
     
@@ -25,7 +26,7 @@ class ReviewService:
         ids = [int(r["ReviewtID"]) for r in rows if r["ReviewID"].isdigit()]
         return max(ids, default=0) + 1
 
-    def _already_requested(self, user_id: int, isbn: str) -> bool:
+    def _already_reviewed(self, user_id: int, isbn: str) -> bool:
         """
         Checks if this user has already review the same book 
         Steps:
@@ -37,4 +38,29 @@ class ReviewService:
         rows = self.repo.read_all(self.path)
         return any(r["UserID"] == str(user_id) and r["ISBN"] == isbn for r in rows)
 
+    
+    def create_review(self, user_id: int, data: ReviewCreate) -> ReviewRead:
+        next_id = self.__generate_next_id()
+        if self._already_reviewed(user_id, data.isbn):
+            raise ValueError("This user has already reviewed this book.")
         
+        review = Review(
+            review_id= next_id,
+            user_id= user_id,
+            isbn= data.isbn,
+            comment= data.comment
+        )
+        
+        self.repo.append_row(self.path, self.fields, review.to_csv_dict())
+        return ReviewRead(**review.to_api_dict())
+    
+    def get_all_reviews(self, isbn: str) -> list[ReviewRead]:
+        rows = self.repo.read_all(self.path) 
+        filtered_reviews = [r for r in rows if r["ISBN"] == isbn]
+        return [ReviewRead(**Review.from_dict(r).to_api_dict()) for r in filtered_reviews]
+
+
+    def edit_review(self, data: ReviewUpdate) -> ReviewRead:
+        rows = self.repo.read_all(self.path)
+        
+        pass
---
+++ backend/tests/unit_testing/test_request.py
@@ -0,0 +1,19 @@
+import pytest
+from app.services.request_service import RequestService
+
+service = RequestService()
+
+def test_create_request_success():
+    pass
+
+def test_prevent_duplicate_request():
+    pass
+
+def test_generate_request_id_incrementally():
+    pass
+
+def test_get_all_requests_returns_list():
+    pass
+
+def test_delete_request_reindexes_ids():
+    pass
