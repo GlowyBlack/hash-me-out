f7d9246e92cd6f73927e608117f934ccce2aa9bf
Laura M <194057493+lmckeelv@users.noreply.github.com>
2025-10-28 12:03:32 -0700

    Updated rating schema (changed rating so it's 0-10)
    Forgot rating was out of 10, and not a 5 star system.
    
    Also, you should be able to rate something 0/10.

--- backend/app/schemas/rating.py
+++ backend/app/schemas/rating.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 class RatingCreate(BaseModel):
     isbn: str = Field(..., min_length=10, max_length=13)
-    rating: int = Field(..., ge=1, le=5)
+    rating: int = Field(..., ge=0, le=10)
 
 class RatingRead(BaseModel):
     user_id: int = Field(..., gt=0) # field is required, id must be greater than 0
